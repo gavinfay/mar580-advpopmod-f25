93a594a895d4e531624d92594e8c65d6aefce5b8
gavinfay <gfay42@gmail.com>
2025-10-02 12:25:32 -0400

    adds state space 2 and hw 2

---
+++ materials/2025-10-02/lingcod.R
@@ -0,0 +1,45 @@
+# schnute lingcod example
+
+## Load TMB TRUE
+library(RTMB)
+library(tidyverse)
+
+
+## Data(and parameters
+lingcod <- read_table("lingcod.dat",skip=7)
+data <-list(obs=log(lingcod$`#data`))
+
+parameters <- list(logr=0, logit_beta=0, log_sigma_proc=0, log_sigma_obs=0,
+                   eta=rep(0,nrow(lingcod)-1))
+## Model function
+lingcod <- function(parms) {
+  #make things visible
+  
+  #transform parameters
+  
+  #initialize section (storage variables, etc.)
+  
+  #process model
+   # set initial biomass
+   # loop over time
+      # predict expected biomass 
+      # do process error model - compare prediction to state
+  
+  #observation model
+  
+  #report_section
+  
+}
+
+
+## Make a function object
+obj <- MakeADFun(lingcod, parameters, random = "eta") #need the random effects
+
+## Call function minimizer
+opt <-nlminb(obj$par, obj$fn, obj$gr)
+
+## Get parameter uncertainties and convergence diagnostics
+sdr <- sdreport(obj)
+sdr
+summary(sdr)
+
