96b76525b935346a9ed801ab1d6b9978888a9747
gavinfay <gfay42@gmail.com>
2025-11-13 15:37:55 -0500

    adds YPR & SBPR functions

---
+++ materials/2025-11-13/SPR_calcs.R
@@ -0,0 +1,40 @@
+nages <- scan("data/floundah_biology.txt",n=1,skip=9)
+maturity <- scan("data/floundah_biology.txt",n=nages,skip=11)
+selex <- scan("data/floundah_biology.txt",n=nages,skip=13)
+weight <- scan("data/floundah_biology.txt",n=nages,skip=15)
+
+M = 0.4
+
+YPR <- function(F = 0, M = 0.4, selex, weight) {
+  nages <- length(weight)
+  N <- rep(1, nages)
+  Z <- F*selex+M
+  for (i in 2:nages)
+    N[i] <- N[i-1]*exp(-Z[i-1])
+  N[nages] <- N[nages]/(1-exp(-Z[i]))
+
+  Y = sum(weight * N * selex * F * (1 - exp(-Z)) / Z)  
+  return(Y) 
+}
+#example call
+YPR(F = 1, M = 0.4, selex = selex, weight = weight)
+
+SBPR <- function(F = 0, M = 0.4, selex, weight, maturity) {
+  nages <- length(weight)
+  N <- rep(1, nages)
+  Z <- F*selex+M
+  for (i in 2:nages)
+    N[i] <- N[i-1]*exp(-Z[i-1])
+  N[nages] <- N[nages]/(1-exp(-Z[i]))
+  
+  SBPR <- sum(weight * maturity * N)
+  return(SBPR)  
+}
+SBPR(F = 1, M = 0.4, selex = selex, weight = weight, maturity = maturity)
+
+
+
+
+
+
+
